bee429b7ab6a64b83e0724c60746bd6ba5b12bb1
Theo Louvel <louveltheophile@gmail.com>
2022-08-08 08:37:22 +0200

    wiring up basic io

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,22 @@
 use std::env;
+use std::{io,fs};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    println!("{}", args.len());
 
     match args.len() {
-        1 => println!("Scanning input!"),
-        2 => println!("Scanning file!"),
+        1 => {
+            let mut line = String::new();
+            io::stdin()
+                .read_line(&mut line)
+                .expect("Could not read input.");
+            println!("Here's the line: {}", line);
+        },
+        2 => {
+            let path = &args[1];
+            let source = fs::read_to_string(path).expect("No such file.");
+            println!("Here's the source: {}", source);
+        },
         _ => panic!("Please either provide a file path or no arguments"),
-    }
+    };
 }
