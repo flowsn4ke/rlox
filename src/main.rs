10f7c7db06f5d113a854c64b921d186f68cd4048
Theo Louvel <louveltheophile@gmail.com>
2022-08-10 00:43:01 +0200

    scanner implementation start

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::env;
-use std::{io,fs};
 use std::io::Write;
+use std::{fs, io};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -8,17 +8,17 @@ fn main() {
     match args.len() {
         1 => {
             run_prompt();
-        },
+        }
         2 => {
             let path = &args[1];
             run_file(path);
-        },
+        }
         _ => panic!("Please either provide a file path or no arguments"),
     };
 }
 
 fn run(source: String) {
-    println!("{}", source);
+    let tokens = scanTokens(source);
 }
 
 fn run_file(path: &str) {
@@ -38,9 +38,65 @@ fn run_prompt() {
         io::stdin()
             .read_line(&mut source)
             .expect("Could not read input.");
-   
+
         source = String::from(source.trim_end());
 
         run(source);
     }
+}
+
+fn scanTokens(source: String) -> String {
+    for (i, c) in source.chars().enumerate() {
+        println!("{} at position {}", c, i);
+    } 
+    source
+}
+
+enum TokenType {
+    LEFT_PAREN,
+    RIGHT_PAREN,
+    LEFT_BRACE,
+    RIGHT_BRACE,
+    COMMA,
+    DOT,
+    MINUS,
+    PLUS,
+    SEMICOLON,
+    SLASH,
+    STAR,
+    BANG,
+    BANG_EQUAL,
+    EQUAL,
+    EQUAL_EQUAL,
+    GREATER,
+    GREATER_EQUAL,
+    LESS,
+    LESS_EQUAL,
+    IDENTIFIER,
+    STRING,
+    NUMBER,
+    AND,
+    CLASS,
+    ELSE,
+    FALSE,
+    FUN,
+    FOR,
+    IF,
+    NIL,
+    OR,
+    PRINT,
+    RETURN,
+    SUPER,
+    THIS,
+    TRUE,
+    VAR,
+    WHILE,
+    EOF,
+}
+
+struct Token<T> {
+    typ: TokenType,
+    lexeme: String,
+    literal: T,
+    line: i32,
 }
