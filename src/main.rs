6b6592cad0792b252eb836e349b8c2428132c5f2
Theo Louvel <louveltheophile@gmail.com>
2022-08-21 15:26:55 +0200

    refactoring scanner

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
-use std::env;
 use std::io::Write;
-use std::{fs, io};
+use std::{env, fmt, fs, io};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -18,7 +17,8 @@ fn main() {
 }
 
 fn run(source: String) {
-    let tokens = scanTokens(source);
+    let scanner = Scanner::from(source);
+    let tokens = scanner.scan_tokens();
 }
 
 fn run_file(path: &str) {
@@ -39,64 +39,157 @@ fn run_prompt() {
             .read_line(&mut source)
             .expect("Could not read input.");
 
-        source = String::from(source.trim_end());
-
         run(source);
     }
 }
 
-fn scanTokens(source: String) -> String {
-    for (i, c) in source.chars().enumerate() {
-        println!("{} at position {}", c, i);
-    } 
-    source
+struct Scanner {
+    source: String,
+    start: u32,
+    current: u32,
+    line: u32,
 }
 
+impl Scanner {
+    fn from(source: String) -> Scanner {
+        Scanner {
+            source,
+            start: 0,
+            current: 0,
+            line: 1,
+        }
+    }
+    fn scan_tokens(&self) -> Vec<Token> {
+        println!("test");
+
+        let tokens: Vec<Token> = Vec::new();
+
+        tokens
+        // let mut iter = self.source.chars().peekable();
+
+        // while let Some(chr) = iter.next() {
+        //     println!("{} at position {}", chr, current_idx);
+        //     current_idx += 1;
+
+        //     match chr {
+        //         '(' => tokens.push(Token {
+        //             typ: TokenType::LEFT_PAREN,
+        //             line: current_line,
+        //             lexeme: "lexeme",
+        //             literal: "literal",
+        //         }),
+        //         _ => {}
+        //     };
+
+        //     match iter.peek() {
+        //         Some(x) => {
+        //             println!(" Next is {:?}", *x)
+        //         }
+        //         _ => {
+        //             break;
+        //         }
+        //     };
+        // }
+
+        // println!("Tokens: {:?}", tokens);
+
+        // tokens
+    }
+}
+
+#[derive(Debug)]
+struct Token<'a> {
+    typ: TokenType,
+    lexeme: &'a str,
+    literal: &'a str,
+    line: u32,
+}
+
+impl<'a> Token<'a> {
+    // This is an associated function
+    fn new(typ: TokenType, lexeme: &'a str, literal: &'a str, line: u32) -> Token<'a> {
+        Token {
+            typ,
+            lexeme,
+            literal,
+            line,
+        }
+    }
+    // This is a method
+    fn to_string(&self) -> String {
+        format!("{} {} {}", self.typ, self.lexeme, self.literal)
+    }
+}
+
+// TODO: Implement a function to return a value for each token type?
+// That way we can implement the Add trait on TokenType
+
+fn get_token_type(identifier: &str) -> TokenType {
+    match identifier {
+        "and" => TokenType::AND,
+        "class" => TokenType::CLASS,
+        "else" => TokenType::ELSE,
+        "false" => TokenType::FALSE,
+        "for" => TokenType::FOR,
+        "if" => TokenType::IF,
+        "nil" => TokenType::NIL,
+        "or" => TokenType::OR,
+        "print" => TokenType::PRINT,
+        "return" => TokenType::RETURN,
+        "super" => TokenType::SUPER,
+        "this" => TokenType::THIS,
+        "true" => TokenType::TRUE,
+        "var" => TokenType::VAR,
+        "while" => TokenType::WHILE,
+        _ => TokenType::IDENTIFIER,
+    }
+}
+
+impl fmt::Display for TokenType {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
+
+#[derive(Debug)]
+#[allow(non_camel_case_types)]
 enum TokenType {
-    LEFT_PAREN,
-    RIGHT_PAREN,
-    LEFT_BRACE,
-    RIGHT_BRACE,
+    AND,
+    BANG,
+    BANG_EQUAL,
+    CLASS,
     COMMA,
     DOT,
+    ELSE,
+    EQUAL,
+    EQUAL_EQUAL,
+    FALSE,
+    FOR,
+    FUN,
+    GREATER,
+    GREATER_EQUAL,
+    IDENTIFIER,
+    IF,
+    LEFT_BRACE,
+    LEFT_PAREN,
+    LESS,
+    LESS_EQUAL,
+    NIL,
+    NUMBER,
+    OR,
     MINUS,
     PLUS,
+    PRINT,
+    RETURN,
+    RIGHT_PAREN,
+    RIGHT_BRACE,
     SEMICOLON,
     SLASH,
     STAR,
-    BANG,
-    BANG_EQUAL,
-    EQUAL,
-    EQUAL_EQUAL,
-    GREATER,
-    GREATER_EQUAL,
-    LESS,
-    LESS_EQUAL,
-    IDENTIFIER,
     STRING,
-    NUMBER,
-    AND,
-    CLASS,
-    ELSE,
-    FALSE,
-    FUN,
-    FOR,
-    IF,
-    NIL,
-    OR,
-    PRINT,
-    RETURN,
     SUPER,
     THIS,
     TRUE,
     VAR,
     WHILE,
-    EOF,
-}
-
-struct Token<T> {
-    typ: TokenType,
-    lexeme: String,
-    literal: T,
-    line: i32,
 }
