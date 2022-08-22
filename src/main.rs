654108c66e2308002fc52ea0edab912bc40d75de
Theo Louvel <louveltheophile@gmail.com>
2022-08-22 22:17:38 +0200

    switched tokens to enum instead of struct, more idiomatic and easier to read / reason about

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::io::Write;
-use std::{env, fmt, fs, io};
+use std::{env, fs, io};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -46,7 +46,7 @@ fn run_prompt() {
 struct Scanner {
     source: Vec<char>,
     current: usize,
-    line: u32,
+    line: usize,
 }
 
 impl Scanner {
@@ -57,8 +57,8 @@ impl Scanner {
             line: 1,
         }
     }
-    fn scan_tokens(&mut self) -> Vec<TokenTypes> {
-        let mut tokens: Vec<TokenTypes> = Vec::new();
+    fn scan_tokens(&mut self) -> Vec<Token> {
+        let mut tokens: Vec<Token> = Vec::new();
 
         while self.is_not_at_end() {
             let c = self.get_current_char();
@@ -94,9 +94,9 @@ impl Scanner {
                     _ => tokens.push(self.scan_symbol(1)),
                 },
                 '"' => tokens.push(self.scan_string()),
-                '/' =>  match self.peek() {
+                '/' => match self.peek() {
                     '/' => self.skip_comment(),
-                    _ => tokens.push(self.scan_symbol(1))
+                    _ => tokens.push(self.scan_symbol(1)),
                 },
                 '\n' => {
                     self.line += 1;
@@ -108,7 +108,8 @@ impl Scanner {
                     } else if self.is_alpha(c) {
                         tokens.push(self.scan_identifier());
                     } else {
-                        panic!("Unexpected character.");
+                        // TODO: Don't panic, output error message and keep scanning so we can report multiple errors at once.
+                        panic!("Unexpected character at line {}", self.line);
                     }
                 }
             };
@@ -128,6 +129,9 @@ impl Scanner {
     fn peek(&self) -> char {
         self.source[self.current + 1]
     }
+    fn is_at_end(&self) -> bool {
+        self.current >= self.source.len()
+    }
     fn is_not_at_end(&self) -> bool {
         self.current < self.source.len()
     }
@@ -139,7 +143,7 @@ impl Scanner {
             self.advance();
         }
     }
-    fn scan_symbol(&mut self, len: usize) -> TokenTypes {
+    fn scan_symbol(&mut self, len: usize) -> Token {
         let start = self.current;
 
         for i in 0..len {
@@ -148,11 +152,10 @@ impl Scanner {
 
         let lexeme_chars = &self.source[start..self.current];
         let lexeme = String::from_iter(lexeme_chars);
-        let typ = get_symbol_type(&lexeme);
 
-        TokenTypes::Symbol(Token::new(typ, lexeme.clone(), lexeme.clone(), self.line))
+        Token::get_token_from_symbol(&lexeme, self.line)
     }
-    fn scan_identifier(&mut self) -> TokenTypes {
+    fn scan_identifier(&mut self) -> Token {
         let start = self.current;
 
         while self.get_current_char().is_alphanumeric() {
@@ -161,12 +164,12 @@ impl Scanner {
 
         let lexeme_chars = &self.source[start..self.current];
         let lexeme = String::from_iter(lexeme_chars);
-        let typ = get_identifier_type(&lexeme);
 
-        TokenTypes::Identifier(Token::new(typ, lexeme.clone(), lexeme.clone(), self.line))
+        Token::get_token_from_identifier(&lexeme, self.line)
     }
-    fn scan_number(&mut self) -> TokenTypes {
+    fn scan_number(&mut self) -> Token {
         let start = self.current;
+        let mut found_decimal = false;
 
         while self.get_current_char().is_digit(10) {
             if !self.peek().is_digit(10) && self.peek().is_alphabetic() {
@@ -175,18 +178,18 @@ impl Scanner {
 
             self.advance();
 
-            if self.get_current_char() == '.' {
+            if self.get_current_char() == '.' && !found_decimal {
                 self.advance(); // add the point to the lexeme to make a float
+                found_decimal = true;
             }
         }
 
         let lexeme_chars = &self.source[start..self.current];
         let lexeme = String::from_iter(lexeme_chars);
-        let number = lexeme.parse::<f64>().unwrap();
 
-        TokenTypes::Number(Token::new(TokenType::Number, lexeme, number, self.line))
+        Token::get_token_from_number(&lexeme, self.line)
     }
-    fn scan_string(&mut self) -> TokenTypes {
+    fn scan_string(&mut self) -> Token {
         let start = self.current;
 
         self.advance(); // skip the first " char
@@ -204,138 +207,114 @@ impl Scanner {
         let lexeme_chars = &self.source[start..self.current];
         let lexeme = String::from_iter(lexeme_chars);
 
-        TokenTypes::String(Token::new(
-            TokenType::String,
-            lexeme.clone(),
-            lexeme.clone(),
-            self.line,
-        ))
+        Token::get_token_from_string(&lexeme, self.line)
     }
 }
 
 #[derive(Debug)]
-struct Token<T: fmt::Display + fmt::Debug> {
-    typ: TokenType,
-    lexeme: String,
-    literal: T,
-    line: u32,
+enum Token {
+    And(String, String, usize),
+    Bang(String, String, usize),
+    BangEqual(String, String, usize),
+    Class(String, String, usize),
+    Comma(String, String, usize),
+    Dot(String, String, usize),
+    Else(String, String, usize),
+    Equal(String, String, usize),
+    EqualEqual(String, String, usize),
+    False(String, String, usize),
+    For(String, String, usize),
+    Fun(String, String, usize),
+    Greater(String, String, usize),
+    GreaterEqual(String, String, usize),
+    Identifier(String, String, usize),
+    If(String, String, usize),
+    LeftBracket(String, String, usize),
+    LeftBrace(String, String, usize),
+    LeftParen(String, String, usize),
+    Less(String, String, usize),
+    LessEqual(String, String, usize),
+    Nil(String, String, usize),
+    Number(String, f64, usize),
+    Or(String, String, usize),
+    Minus(String, String, usize),
+    Plus(String, String, usize),
+    Print(String, String, usize),
+    Return(String, String, usize),
+    RightParen(String, String, usize),
+    RightBracket(String, String, usize),
+    RightBrace(String, String, usize),
+    Semicolon(String, String, usize),
+    Slash(String, String, usize),
+    Star(String, String, usize),
+    String(String, String, usize),
+    Super(String, String, usize),
+    This(String, String, usize),
+    True(String, String, usize),
+    Var(String, String, usize),
+    While(String, String, usize),
 }
 
-#[derive(Debug)]
-enum TokenTypes {
-    Symbol(Token<String>),
-    Identifier(Token<String>),
-    Number(Token<f64>),
-    String(Token<String>),
-}
+/*
+You should always be using to_owned(). to_string() is the generic conversion
+to a String from any type implementing the ToString trait. It uses the formatting
+functions and therefor might end up doing multiple allocations and running much more
+code than a simple to_owned() which just allocates a buffer and copies the literal
+into the buffer.
+*/
 
-impl<T: fmt::Display + fmt::Debug> Token<T> {
-    fn new(typ: TokenType, lexeme: String, literal: T, line: u32) -> Token<T> {
-        Token {
-            typ,
-            lexeme,
-            literal,
-            line,
+impl Token {
+    fn get_token_from_symbol(lexeme: &str, line: usize) -> Token {
+        match lexeme {
+            "=" => Token::Equal(lexeme.to_owned(), lexeme.to_owned(), line),
+            "==" => Token::EqualEqual(lexeme.to_owned(), lexeme.to_owned(), line),
+            "!=" => Token::BangEqual(lexeme.to_owned(), lexeme.to_owned(), line),
+            ">=" => Token::GreaterEqual(lexeme.to_owned(), lexeme.to_owned(), line),
+            "<=" => Token::LessEqual(lexeme.to_owned(), lexeme.to_owned(), line),
+            "!" => Token::Bang(lexeme.to_owned(), lexeme.to_owned(), line),
+            "-" => Token::Minus(lexeme.to_owned(), lexeme.to_owned(), line),
+            "+" => Token::Plus(lexeme.to_owned(), lexeme.to_owned(), line),
+            ";" => Token::Semicolon(lexeme.to_owned(), lexeme.to_owned(), line),
+            "," => Token::Comma(lexeme.to_owned(), lexeme.to_owned(), line),
+            "." => Token::Dot(lexeme.to_owned(), lexeme.to_owned(), line),
+            "<" => Token::Less(lexeme.to_owned(), lexeme.to_owned(), line),
+            ">" => Token::Greater(lexeme.to_owned(), lexeme.to_owned(), line),
+            "*" => Token::Star(lexeme.to_owned(), lexeme.to_owned(), line),
+            "/" => Token::Slash(lexeme.to_owned(), lexeme.to_owned(), line),
+            "{" => Token::LeftBrace(lexeme.to_owned(), lexeme.to_owned(), line),
+            "}" => Token::RightBrace(lexeme.to_owned(), lexeme.to_owned(), line),
+            "[" => Token::LeftBracket(lexeme.to_owned(), lexeme.to_owned(), line),
+            "]" => Token::RightBracket(lexeme.to_owned(), lexeme.to_owned(), line),
+            "(" => Token::LeftParen(lexeme.to_owned(), lexeme.to_owned(), line),
+            ")" => Token::RightParen(lexeme.to_owned(), lexeme.to_owned(), line),
+            _ => panic!("Unexpected symbol."),
         }
     }
-}
-
-fn get_symbol_type(lexeme: &str) -> TokenType {
-    match lexeme {
-        "=" => TokenType::Equal,
-        "==" => TokenType::EqualEqual,
-        "!=" => TokenType::BangEqual,
-        ">=" => TokenType::GreaterEqual,
-        "<=" => TokenType::LessEqual,
-        "!" => TokenType::Bang,
-        "-" => TokenType::Minus,
-        "+" => TokenType::Plus,
-        ";" => TokenType::Semicolon,
-        "," => TokenType::Comma,
-        "." => TokenType::Dot,
-        "<" => TokenType::Less,
-        ">" => TokenType::Greater,
-        "*" => TokenType::Star,
-        "/" => TokenType::Slash,
-        "{" => TokenType::LeftBrace,
-        "}" => TokenType::RightBrace,
-        "[" => TokenType::LeftBracket,
-        "]" => TokenType::RightBracket,
-        "(" => TokenType::LeftParen,
-        ")" => TokenType::RightParen,
-        _ => panic!("Unexpected symbol."),
+    fn get_token_from_identifier(lexeme: &str, line: usize) -> Token {
+        match lexeme {
+            "and" => Token::And(lexeme.to_owned(), lexeme.to_owned(), line),
+            "class" => Token::Class(lexeme.to_owned(), lexeme.to_owned(), line),
+            "else" => Token::Else(lexeme.to_owned(), lexeme.to_owned(), line),
+            "false" => Token::False(lexeme.to_owned(), lexeme.to_owned(), line),
+            "for" => Token::For(lexeme.to_owned(), lexeme.to_owned(), line),
+            "fun" => Token::Fun(lexeme.to_owned(), lexeme.to_owned(), line),
+            "if" => Token::If(lexeme.to_owned(), lexeme.to_owned(), line),
+            "nil" => Token::Nil(lexeme.to_owned(), lexeme.to_owned(), line),
+            "or" => Token::Or(lexeme.to_owned(), lexeme.to_owned(), line),
+            "print" => Token::Print(lexeme.to_owned(), lexeme.to_owned(), line),
+            "return" => Token::Return(lexeme.to_owned(), lexeme.to_owned(), line),
+            "super" => Token::Super(lexeme.to_owned(), lexeme.to_owned(), line),
+            "this" => Token::This(lexeme.to_owned(), lexeme.to_owned(), line),
+            "true" => Token::True(lexeme.to_owned(), lexeme.to_owned(), line),
+            "var" => Token::Var(lexeme.to_owned(), lexeme.to_owned(), line),
+            "while" => Token::While(lexeme.to_owned(), lexeme.to_owned(), line),
+            _ => Token::Identifier(lexeme.to_owned(), lexeme.to_owned(), line),
+        }
     }
-}
-
-fn get_identifier_type(identifier: &str) -> TokenType {
-    match identifier {
-        "and" => TokenType::And,
-        "class" => TokenType::Class,
-        "else" => TokenType::Else,
-        "false" => TokenType::False,
-        "for" => TokenType::For,
-        "fun" => TokenType::Fun,
-        "if" => TokenType::If,
-        "nil" => TokenType::Nil,
-        "or" => TokenType::Or,
-        "print" => TokenType::Print,
-        "return" => TokenType::Return,
-        "super" => TokenType::Super,
-        "this" => TokenType::This,
-        "true" => TokenType::True,
-        "var" => TokenType::Var,
-        "while" => TokenType::While,
-        _ => TokenType::Identifier,
+    fn get_token_from_number(lexeme: &str, line: usize) -> Token {
+        Token::Number(lexeme.to_owned(), lexeme.parse::<f64>().unwrap(), line)
     }
-}
-
-impl fmt::Display for TokenType {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{:?}", self)
+    fn get_token_from_string(lexeme: &str, line: usize) -> Token {
+        Token::String(lexeme.to_owned(), lexeme.to_owned(), line)
     }
-}
-
-#[derive(Debug)]
-#[allow(non_camel_case_types)]
-enum TokenType {
-    And,
-    Bang,
-    BangEqual,
-    Class,
-    Comma,
-    Dot,
-    Else,
-    Equal,
-    EqualEqual,
-    False,
-    For,
-    Fun,
-    Greater,
-    GreaterEqual,
-    Identifier,
-    If,
-    LeftBracket,
-    LeftBrace,
-    LeftParen,
-    Less,
-    LessEqual,
-    Nil,
-    Number,
-    Or,
-    Minus,
-    Plus,
-    Print,
-    Return,
-    RightParen,
-    RightBracket,
-    RightBrace,
-    Semicolon,
-    Slash,
-    Star,
-    String,
-    Super,
-    This,
-    True,
-    Var,
-    While,
 }
